df79255dce078c81ad478cd03411d6920cd32c01
Rehan <rehanlemu@gmail.com>
2026-01-26 19:23:26 +0600

    create donorSchema

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const app = express();
-const port = process.env.PORT || 5000
+import express from 'express';
+import cors from 'cors';
+import dbConnect from "./config/mongodb.js";
+import "dotenv/config";
 
-// MiddleWare
+import userRouter from './routes/userRouter.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// DB connect
+dbConnect();
+
+// Middleware
 app.use(cors());
-app.use(cors({
-//   origin: 'https://medimart-cbe0f.web.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-res.send(`server is running `);
-})
+  res.send('server is running');
+});
+
+app.use('/api/user', userRouter)
+
+
+
 
 app.listen(port, () => {
-console.log(`Coffee server is running by port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
