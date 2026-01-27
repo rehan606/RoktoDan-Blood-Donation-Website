07e8e35c702232de4e90f90b6ff5753bdd54963b
Rehan <rehanlemu@gmail.com>
2026-01-28 00:32:19 +0600

    Remove previews Backend file and create new Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import dbConnect from "./config/mongodb.js";
-import "dotenv/config";
-
-import donorRouter from './routes/donorRouter.js';
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // DB connect
-dbConnect();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('server is running');
+  res.send('Server is running');
 });
 
-app.use('/api/donor', donorRouter)
 
 
 
