7758cff946b586f4579c0fbe681cd7b8f479db55
Rehan <rehanlemu@gmail.com>
2026-01-31 01:36:26 +0600

    setup firebase admin

--- Backend/index.js
+++ Backend/index.js
@@ -17,8 +17,19 @@ app.use(express.json());
 const serviceAccount = require("./firebase-admin-key.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
+
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+
 
 
   // ========= MongoDB Driver =========
