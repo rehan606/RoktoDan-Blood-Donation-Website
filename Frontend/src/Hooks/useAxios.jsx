191c0888ec1c675575fc37be88d8a8fb3657e301
Rehan <rehanlemu@gmail.com>
2026-01-30 21:52:25 +0600

    create useAxios in Frontend

--- Frontend/src/Hooks/useAxios.jsx
+++ Frontend/src/Hooks/useAxios.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import axios from "axios"
+
+const axiosInstance = axios.create({
+    baseURL :  `http://localhost:5000`
+})
 
 const useAxios = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return axiosInstance;
 }
 
 export default useAxios
