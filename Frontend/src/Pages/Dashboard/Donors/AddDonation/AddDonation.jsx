7903d634d66d16c44d1b13f59fc51d9ad02ef402
Rehan <rehanlemu@gmail.com>
2026-02-13 01:24:31 +0600

    design add donation form

--- Frontend/src/Pages/Dashboard/Donors/AddDonation/AddDonation.jsx
+++ Frontend/src/Pages/Dashboard/Donors/AddDonation/AddDonation.jsx
@@ -3,176 +3,185 @@ import Swal from "sweetalert2";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 import useAuth from "../../../../Hooks/useAuth";
 import {useLanguage} from "../../../../context/LanguageContext";
+import useBloodGroups from "../../../../Hooks/useBloodGroups";
 
 const AddDonation = () => {
-  const axiosSecure = useAxiosSecure();
-  const { user } = useAuth();
-  const { language } = useLanguage();
+    const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
+    const { language } = useLanguage();
+    const { bloodGroups } = useBloodGroups()
 
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
 
-    const form = e.target;
+        const form = e.target;
 
-    const donationData = {
-      donorId: user?.uid, // অথবা তোমার donor _id
-      bloodGroup: form.bloodGroup.value,
-      patientName: form.patientName.value,
-      hospitalName: form.hospitalName.value,
-      donationType: form.donationType.value,
-      donatedAt: form.donatedAt.value,
+        const donationData = {
+        donorId: user?.uid, // অথবা তোমার donor _id
+        bloodGroup: form.bloodGroup.value,
+        patientName: form.patientName.value,
+        hospitalName: form.hospitalName.value,
+        donationType: form.donationType.value,
+        donatedAt: form.donatedAt.value,
+        };
+
+        try {
+        const res = await axiosSecure.post(
+            "/blood-donations",
+            donationData
+        );
+
+        if (res.data.success) {
+            Swal.fire({
+            icon: "success",
+            title:
+                language === "bn"
+                ? "সফলভাবে সাবমিট হয়েছে!"
+                : "Submitted Successfully!",
+            text:
+                language === "bn"
+                ? "আপনার ডোনেশন রিকোয়েস্ট এডমিন অনুমোদনের জন্য পাঠানো হয়েছে।"
+                : "Your donation is sent for admin approval.",
+            });
+
+            form.reset();
+        }
+        } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title:
+            language === "bn"
+                ? "ত্রুটি হয়েছে!"
+                : "Error!",
+            text:
+            error?.response?.data?.message ||
+            (language === "bn"
+                ? "কিছু ভুল হয়েছে"
+                : "Something went wrong"),
+        });
+        }
+
+        setLoading(false);
     };
 
-    try {
-      const res = await axiosSecure.post(
-        "/blood-donations",
-        donationData
-      );
+    return (
+        <div className="max-w-3xl mx-auto px-4 py-8">
+        <div className="bg-white shadow-lg rounded-xl p-6 md:p-10">
+            <h2 className="text-2xl md:text-3xl font-bold  mb-6 text-[#0C2349]">
+            {language === "bn"
+                ? "নতুন রক্তদান যুক্ত করুন"
+                : "Add New Donation"}
+            </h2>
 
-      if (res.data.success) {
-        Swal.fire({
-          icon: "success",
-          title:
-            language === "bn"
-              ? "সফলভাবে সাবমিট হয়েছে!"
-              : "Submitted Successfully!",
-          text:
-            language === "bn"
-              ? "আপনার ডোনেশন রিকোয়েস্ট এডমিন অনুমোদনের জন্য পাঠানো হয়েছে।"
-              : "Your donation is sent for admin approval.",
-        });
+            <form onSubmit={handleSubmit} className="space-y-5">
 
-        form.reset();
-      }
-    } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title:
-          language === "bn"
-            ? "ত্রুটি হয়েছে!"
-            : "Error!",
-        text:
-          error?.response?.data?.message ||
-          (language === "bn"
-            ? "কিছু ভুল হয়েছে"
-            : "Something went wrong"),
-      });
-    }
+                <div className=" flex items-center justify-between gap-6">
+                    {/* Patient Name */}
+                    <div className="flex flex-col flex-1">
+                        <label className="block mb-1 font-medium text-zinc-800">
+                        {language === "bn" ? "রোগীর নাম" : "Patient Name"}
+                        </label>
+                        <input
+                        type="text"
+                        name="patientName"
+                        placeholder={ language === "bn" ? "রোগীর নাম " : "Enter Patient Name"}
+                        required
+                        className="w-full border border-gray-300 bg-[#FAFAFA] rounded-sm px-4 py-2 focus:none focus:ring-gray-400 outline-none text-zinc-700"
+                        />
+                    </div>
 
-    setLoading(false);
-  };
+                    {/* Hospital */}
 
-  return (
-    <div className="max-w-3xl mx-auto px-4 py-8">
-      <div className="bg-white shadow-lg rounded-xl p-6 md:p-10">
-        <h2 className="text-2xl md:text-3xl font-bold text-center mb-6 text-red-600">
-          {language === "bn"
-            ? "নতুন রক্তদান যুক্ত করুন"
-            : "Add New Donation"}
-        </h2>
+                    <div className="flex flex-col flex-1">
+                        <label className="block mb-1 font-medium text-zinc-800">
+                        {language === "bn" ? "হাসপাতালের নাম" : "Hospital Name"}
+                        </label>
+                        <input
+                        type="text"
+                        name="hospitalName"
+                        placeholder={ language === "bn" ? " হাসপাতালের নাম" : "Enter Hospital Name"}
+                        required
+                        className="w-full border border-gray-300 bg-[#FAFAFA] rounded-sm px-4 py-2 focus:none focus:ring-gray-400 outline-none text-zinc-700"
+                        />
+                    </div>
 
-        <form onSubmit={handleSubmit} className="space-y-5">
+                </div>
 
-          {/* Blood Group */}
-          <div>
-            <label className="block mb-1 font-medium">
-              {language === "bn" ? "রক্তের গ্রুপ" : "Blood Group"}
-            </label>
-            <select
-              name="bloodGroup"
-              required
-              className="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-red-400 outline-none"
-            >
-              <option value="">Select</option>
-              <option value="A+">A+</option>
-              <option value="A-">A-</option>
-              <option value="B+">B+</option>
-              <option value="B-">B-</option>
-              <option value="O+">O+</option>
-              <option value="O-">O-</option>
-              <option value="AB+">AB+</option>
-              <option value="AB-">AB-</option>
-            </select>
-          </div>
+                <div className=" flex items-center justify-between gap-6">
+                    
+                    {/* Blood Group */}
+                    <div className="flex flex-col flex-1">
+                        <label className="block mb-1 font-medium text-zinc-800">
+                        {language === "bn" ? "রক্তের গ্রুপ" : "Blood Group"}
+                        </label>
+                        <select
+                        name="bloodGroup"
+                        required
+                        className="w-full border border-gray-300 bg-[#FAFAFA] rounded-sm px-4 py-2 focus:none focus:ring-gray-400 outline-none text-zinc-700"
+                        >
+                            <option value="">Select</option>
+                            {bloodGroups.map((bg) => (
+                                <option key={bg.lebel} value={bg.value}>
+                                    {bg.value}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    {/* Donation Date */}
+                    <div className="flex flex-col flex-1">
+                        <label className="block mb-1 font-medium text-zinc-800">
+                        {language === "bn" ? "ডোনেশনের তারিখ" : "Donation Date"}
+                        </label>
+                        <input
+                        type="date"
+                        name="donatedAt"
+                        required
+                        className="w-full border border-gray-300 bg-[#FAFAFA] rounded-sm px-4 py-2 focus:none focus:ring-gray-400 outline-none text-zinc-700"
+                        />
+                    </div>
+                </div>
 
-          {/* Patient Name */}
-          <div>
-            <label className="block mb-1 font-medium">
-              {language === "bn" ? "রোগীর নাম" : "Patient Name"}
-            </label>
-            <input
-              type="text"
-              name="patientName"
-              required
-              className="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-red-400 outline-none"
-            />
-          </div>
-
-          {/* Hospital */}
-          <div>
-            <label className="block mb-1 font-medium">
-              {language === "bn" ? "হাসপাতালের নাম" : "Hospital Name"}
-            </label>
-            <input
-              type="text"
-              name="hospitalName"
-              required
-              className="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-red-400 outline-none"
-            />
-          </div>
-
-          {/* Donation Type */}
-          <div>
-            <label className="block mb-1 font-medium">
-              {language === "bn" ? "ডোনেশন টাইপ" : "Donation Type"}
-            </label>
-            <select
-              name="donationType"
-              className="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-red-400 outline-none"
-            >
-              <option value="direct">
-                {language === "bn" ? "ডাইরেক্ট কল" : "Direct Call"}
-              </option>
-              <option value="request">
-                {language === "bn" ? "রিকোয়েস্ট ভিত্তিক" : "Request Based"}
-              </option>
-            </select>
-          </div>
-
-          {/* Donation Date */}
-          <div>
-            <label className="block mb-1 font-medium">
-              {language === "bn" ? "ডোনেশনের তারিখ" : "Donation Date"}
-            </label>
-            <input
-              type="date"
-              name="donatedAt"
-              required
-              className="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-red-400 outline-none"
-            />
-          </div>
-
-          {/* Submit Button */}
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full bg-red-600 text-white py-3 rounded-lg font-semibold hover:bg-red-700 transition duration-300 disabled:opacity-60"
-          >
-            {loading
-              ? language === "bn"
-                ? "অপেক্ষা করুন..."
-                : "Please wait..."
-              : language === "bn"
-              ? "ডোনেশন সাবমিট করুন"
-              : "Submit Donation"}
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+                <div className=" flex items-center justify-between gap-6">
+                    {/* Donation Type */}
+                    <div className="flex flex-col flex-1 ">
+                        <label className="block mb-1 font-medium text-zinc-800">
+                        {language === "bn" ? "ডোনেশন টাইপ" : "Donation Type"}
+                        </label>
+                        <select
+                        name="donationType"
+                    className="w-full border border-gray-300 bg-[#FAFAFA] rounded-sm px-4 py-2 focus:none focus:ring-gray-400 outline-none text-zinc-700"
+                        >
+                        <option value="direct">
+                            {language === "bn" ? "ডাইরেক্ট কল" : "Direct Call"}
+                        </option>
+                        <option value="request">
+                            {language === "bn" ? "রিকোয়েস্ট ভিত্তিক" : "Request Based"}
+                        </option>
+                        </select>
+                    </div>
+                    
+                    {/* Submit Button */}
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className=" flex flex-col flex-1 w-full bg-[#0C2349] text-white py-2.5 rounded-sm font-semibold hover:bg-[#0B2349] transition duration-300 disabled:opacity-60 mt-6 cursor-pointer"
+                    >
+                        {loading
+                        ? language === "bn"
+                            ? "অপেক্ষা করুন..."
+                            : "Please wait..."
+                        : language === "bn"
+                        ? "ডোনেশন সাবমিট করুন"
+                        : "Submit Donation"}
+                    </button>
+                </div>
+            </form>
+        </div>
+        </div>
+    );
 };
 
 export default AddDonation;
